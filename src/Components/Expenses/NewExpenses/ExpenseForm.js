1e4700c2776ada438bc58b686043e82e256ee10c
Shannkar022 <shankarweb22@gmail.com>
2021-09-30 18:55:44 +0530

    adding two way binding

--- src/Components/Expenses/NewExpenses/ExpenseForm.js
+++ src/Components/Expenses/NewExpenses/ExpenseForm.js
@@ -10,31 +10,46 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     setTitle(event.target.value)
-   
-      console.log(enteredInput)
+  
   };
   const amountChangeHandler = (event) => {
     setAmount(event.target.value)
     
   };
   const dateChangeHandler = (event) => {
-    setDate(event.target.value)
+      setDate(event.target.value);
     
-  };
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        const expeneData = {
+            title: title,
+            amount: amount,
+           date: new Date(date)
+        }
+        console.log(expeneData)
+        setTitle('');
+        setAmount('');
+        setDate('');
+    }
+    
+
 
   return (
-    <form>
+    <form  onSubmit={formSubmitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={title} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
             min="0.01"
-            step="0.01"
+                      step="0.01"
+                      value={amount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -43,7 +58,8 @@ const ExpenseForm = () => {
           <input
             type="date"
             min="2019-01-01"
-            max="2022-12-31"
+                      max="2022-12-31"
+                      value={date}
             onChange={dateChangeHandler}
           />
         </div>
