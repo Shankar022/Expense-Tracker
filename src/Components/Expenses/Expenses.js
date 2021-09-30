d9b57465ca1d979731ac44e8c86e274006c9ed14
Shannkar022 <shankarweb22@gmail.com>
2021-09-30 20:10:22 +0530

    ExpenseFilter component added

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,22 +2,33 @@ import React from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "../Expenses/ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./NewExpenses/ExpensesFilter";
 
 const Expenses = ({ items }) => {
+  
+  const getDropDownChangeData = (selectedYear) => {
+   
+    console.log(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {
-        // console.log(items),
-        items.map((item) => (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        ))
-      }
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter
+          onDropDownChange={getDropDownChangeData}
+        />
+        {
+          // console.log(items),
+          items.map((item) => (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          ))
+        }
+      </Card>
+    </>
   );
 };
 
